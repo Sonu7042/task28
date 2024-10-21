134218ed5d77421336a53416d6ed07d4826f7dd8
SonuSingh05 <sonusinghmourya143@gmail.com>
2024-10-21 21:50:53 +0530

    updated

--- db/db.js
+++ db/db.js
@@ -1,7 +1,8 @@
 const mongoose= require('mongoose')
 
 
-const link= "mongodb://localhost:27017/TodoList"
+// const link= "mongodb://localhost:27017/TodoList"
+const link= "mongodb+srv://admin:sonu12345@cluster0.uanic.mongodb.net/"
 
  // useNewUrlParser: true,    
 //  useUnifiedTopology: true
