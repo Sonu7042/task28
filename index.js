218f5db541af54c28fc1dd5be8e36737e39a58ee
SonuSingh05 <sonusinghmourya143@gmail.com>
2024-10-22 07:53:17 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express= require("express")
 const mongoConnect= require('./db/db')
 const router= require('./router/router')
+const cors= require('cors')
 
 
 mongoConnect()
 const app= express()
 app.use(express.json())
+app.use(cors())
 
 
 app.use("/", router)
