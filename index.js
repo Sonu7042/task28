d922078fe0a9f10dd2b4e9c65a21f23f52429d4e
SonuSingh05 <sonusinghmourya143@gmail.com>
2024-10-23 22:15:14 +0530

    updated

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ const cors= require('cors')
 mongoConnect()
 const app= express()
 
-app.use(cors({
-    origin:"https://poetic-medovik-ae7c50.netlify.app/"
-}))
+// app.use(cors({
+//     origin:"https://poetic-medovik-ae7c50.netlify.app/"
+// }))
+
+
+app.use(cors())
+
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
    
-
-
-
 app.use("/", router)
-
-
 
 app.listen(9000, ()=>{
     console.log("server is listening...")
